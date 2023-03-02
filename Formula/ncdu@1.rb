46c1493f463d3f14c2d98c9c0feb6c3e70655dc1
Moon Sungjoon <sumoon@seoulsaram.org>
2023-03-02 22:45:23 +0900

    ncdu@1 1.18.1 (new formula)

---
+++ Formula/ncdu@1.rb
@@ -0,0 +1,26 @@
+class NcduAT1 < Formula
+  desc "NCurses Disk Usage"
+  homepage "https://dev.yorhel.nl/ncdu"
+  url "https://dev.yorhel.nl/download/ncdu-1.18.1.tar.gz"
+  sha256 "7c0fa1eb29d85aaed4ba174164bdbb8f011b5c390d017c57d668fc7231332405"
+  license "MIT"
+
+  keg_only :versioned_formula
+  uses_from_macos "ncurses"
+
+  def install
+    system "autoreconf", "-i" if build.head?
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/ncdu -v")
+    system bin/"ncdu", "-o", "test"
+    output = JSON.parse((testpath/"test").read)
+    assert_equal "ncdu", output[2]["progname"]
+    assert_equal version.to_s, output[2]["progver"]
+    assert_equal Pathname.pwd.size, output[3][0]["asize"]
+  end
+end
